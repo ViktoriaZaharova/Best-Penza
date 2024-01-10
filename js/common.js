f027bd3faa6ac9d1a2de1fc20ad0922fb5629733
Виктория Захарова <zaharova181114@gmail.com>
2024-01-11 00:25:33 +0200

    home section

--- js/common.js
+++ js/common.js
@@ -1,2 +1,28 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+$('.home-bg-slider').slick({
+	slidesToShow: 1,
+	fade: true,
+	arrows: false,
+	swipe: false,
+	infinite: false,
+});
+
+$('.home-slider').slick({
+	slidesToShow: 1,
+	fade: true,
+	arrows: false,
+	dots: true,
+	infinite: false,
+	swipe: false,
+	appendDots: '.home-slider__nav',
+	asNavFor: '.home-bg-slider, .home-image-slider',
+});
+
+$('.home-image-slider').slick({
+	slidesToShow: 1,
+	fade: true,
+	arrows: false,
+	swipe: false,
+	infinite: false,
+});
