22912aed3bce532fe84ab908aa52b65bb0f0476d
Виктория Захарова <zaharova181114@gmail.com>
2024-01-14 18:11:59 +0200

    + 3 pages

--- js/common.js
+++ js/common.js
@@ -31,6 +31,34 @@ $(".home-slider").on('afterChange', function (event, slick, currentSlide) {
 	$(".home-slider__cp").text(currentSlide < 10 ? `0${currentSlide + 1}` : currentSlide + 1);
 });
 
+$('.reviews-slider').slick({
+	slidesToShow: 2,
+	arrows: true,
+	dots: true,
+	appendDots: '.reviews-slider__nav',
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+});
+
+$(".reviews-slider").on('afterChange', function (event, slick, currentSlide) {
+	$(".reviews-slider__cp").text(currentSlide < 10 ? `0${currentSlide + 1}` : currentSlide + 1);
+});
+
 Fancybox.bind("[data-fancybox]", {
 	// Your custom options
+});
+
+$('.form-control').focus(function () {
+	$('.form-group').removeClass('focus');
+	$(this).parents('.form-group').addClass('focus');
+});
+
+$('.btn-toggle-news').on('click', function (e) {
+	e.preventDefault();
+	$(this).parents('.container').find('.card-col:hidden').slice(0, 3).slideDown();
+
+	var onBlock = $(this).parents('.container').find('.card-col:hidden').length;
+	if (onBlock <= 0) {
+		$(this).hide();
+	}
 });
