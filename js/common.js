a8e961ba7d4ccebe906261c9a732dc24a642fbd6
Виктория Захарова <zaharova181114@gmail.com>
2024-01-22 04:32:41 +0200

    +

--- js/common.js
+++ js/common.js
@@ -202,3 +202,14 @@ $('.btn-burger').on('click', function (e) {
 $('.nav-menu__close').on('click', function () {
 	$('.nav-menu').fadeOut();
 });
+
+$(".js-tab-trigger").click(function () {
+	var id = $(this).attr('data-tab'),
+		content = $('.js-tab-content[data-tab="' + id + '"]');
+
+	$('.js-tab-trigger.active').removeClass('active'); // 1
+	$(this).addClass('active'); // 2
+
+	$('.js-tab-content.active').removeClass('active'); // 3
+	content.addClass('active'); // 4
+});
