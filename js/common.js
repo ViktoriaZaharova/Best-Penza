d1452ea66aeaf61e9c8acf7dc25fd357855e2599
Виктория Захарова <zaharova181114@gmail.com>
2024-01-22 02:58:29 +0200

    + article mobile

--- js/common.js
+++ js/common.js
@@ -121,6 +121,19 @@ $('.article-slider').slick({
 	appendArrows: '.article-slider__nav',
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+			}
+		},{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+			}
+		}
+	]
 });
 
 $(".article-slider").on('afterChange', function (event, slick, currentSlide) {
